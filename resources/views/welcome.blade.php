31a34565686de5441f0ee1da0140efb98156a378
rafat69ahmed <rafat69ahmed@gmail.com>
2020-07-18 18:23:15 +0600

    testing cd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Romesh vai boss
                 </div>
 
                 <div class="links">
