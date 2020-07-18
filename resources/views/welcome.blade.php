94c52598d5681e63b9693bb5faae9c22a3eb3d8f
rafat69ahmed <rafat69ahmed@gmail.com>
2020-07-18 18:27:30 +0600

    testing cd 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Romesh vai boss
+                    Romesh vai boss manush
                 </div>
 
                 <div class="links">
